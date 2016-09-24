37c68f766adefda6420684558bef02436ca48904
Jacek Czaja <jacek.czaja@gmail.com>
2016-09-24 14:45:51 +0200

    - Added reading input and painting it and validation of anwser typed

--- equations.py
+++ equations.py
@@ -6,8 +6,7 @@ import sys
 import random
 
 # TODO:
-# - fullscreen
-# - Input field to be done
+# subtracting, multiplying, fibonacci, derivatives
 
 class Equation(QtGui.QWidget):
     def __init__(self):
@@ -15,8 +14,8 @@ class Equation(QtGui.QWidget):
         # Inicjalizacja
         random.seed()
         self.text = self.makeRandomEquation()
-        self.setGeometry(0,0,600,800)
-        self.show()
+        self.lenBaseText = len(self.text)   # length of basic equation (this should be preserved)
+        self.showFullScreen()
     def paintEvent(self,event):
         qp = QtGui.QPainter()
         qp.begin(self)
@@ -25,17 +24,56 @@ class Equation(QtGui.QWidget):
 
     def drawText(self, event, qp):
         qp.setPen(QtGui.QColor(0,0,255))
-        qp.setFont(QtGui.QFont('Decorative',100))
+        qp.setFont(QtGui.QFont('Decorative',200))
         qp.drawText(event.rect(), QtCore.Qt.AlignCenter, self.text)
 
     def makeRandomEquation(self):
-        # TODO: write a body of equation
-        a = random.randint(1,5)
-        b = random.randint(1,5)
-        equation_string=str(a)+"+"+str(b)
+        #TODO: Support for other operations (-, *)
+        self.a = random.randint(1,5)
+        self.b = random.randint(1,5)
+        equation_string=str(self.a)+"+"+str(self.b)+"="
         #print("Equation string %s" %(equation_string))
         return equation_string
 
+    def keyPressEvent(self, e):
+        key2str = {
+                   QtCore.Qt.Key_0 : "0",
+                   QtCore.Qt.Key_1 : "1",
+                   QtCore.Qt.Key_2 : "2",
+                   QtCore.Qt.Key_3 : "3",
+                   QtCore.Qt.Key_4 : "4",
+                   QtCore.Qt.Key_5 : "5",
+                   QtCore.Qt.Key_6 : "6",
+                   QtCore.Qt.Key_7 : "7",
+                   QtCore.Qt.Key_8 : "8",
+                   QtCore.Qt.Key_9 : "9",
+                    }
+        if(e.isAutoRepeat() != True):
+            if((e.key() == QtCore.Qt.Key_Backspace) and (len(self.text) > self.lenBaseText)):
+               self.text = self.text[:-1]         
+            elif((e.key() in key2str) and (len(self.text) < self.lenBaseText + 3)): # No more than three characters
+                self.text+=key2str[e.key()]
+            elif((e.key() == QtCore.Qt.Key_Enter) or (e.key() == QtCore.Qt.Key_Return)):
+                #TODO verify if anwser is ok
+                if(self.validateEquation() == True):
+                    print("SUPER KASIA")
+                else:
+                    print("ZLE!!")
+                
+            self.update()
+
+    def validateEquation(self):
+        # Get result typed and convert it to number
+        if(len(self.text) == self.lenBaseText):
+            return False
+        typed_result = int(self.text[self.lenBaseText:])
+        computed_result = self.a + self.b
+        # compare typed result with computed result
+        if(typed_result == computed_result):
+            return True
+        else:
+            return False
+        
 app = QtGui.QApplication(sys.argv)
 #w = QtGui.QWidget()
 #w.resize(300,400)
